6e0f6f06a763671668b19ae830d086b6ca234dd3
shubham <shubhamkumarrawat@hotmail.com>
2022-03-12 05:30:12 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="normalize.css">
+	<link rel="stylesheet" type="text/css" href="bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	
+	<section id="signupform" class="section_class">
+		<div class="col-sm-12">
+			<div class="col-sm-4"></div>
+			
+			
+			<div class="col-sm-4">
+				<div class="contain_form">
+					<!-- <center> -->
+					<h2>Sign Up</h2>
+					<form id="signupform">
+						<input type="text" id="username" placeholder="User Id"><br><br>
+						<input type="password" id="password" placeholder="Password"><br><br>
+						<input type="password" id="password" placeholder="Confirm Password"><br><br>
+						<input type="submit" value="Submit">
+					</form> 
+					<!-- </center> -->
+				</div>
+				
+			</div>
+			
+			
+			<div class="col-sm-4"></div>
+		</div>
+	</section>
+				
+</body>
+</html>

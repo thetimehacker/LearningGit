dedccdc3d80ca3963bdd7b92b102a554f7fabe60
shubham <shubhamkumarrawat@hotmail.com>
2022-03-12 07:16:06 +0530

    Revert "Added div class to all the elements"
    This reverts commit 9c54db3f5dc87ad6c3f9776471096d6e0c7aab59.
    
    # Conflicts:
    #	style.css

--- index.php
+++ index.php
@@ -20,18 +20,10 @@
 				<div class="contain_form">
 					<h2>Sign Up</h2>
 					<form id="signupform">
-						<div class="input-form">
-							<input type="text" id="username" placeholder="User Id" class="form-control" required><br>
-						</div>
-						<div class="input-form">
-							<input type="password" id="password" placeholder="Password" class="form-control" required><br>
-						</div>
-						<div class="input-form">
-							<input type="password" id="password" placeholder="Confirm Password" class="form-control" required><br>
-						</div>
-						<div class="input-form">
-							<input type="submit" value="Submit" class="form-control">
-						</div>
+						<input type="text" id="username" placeholder="User Id" class="form-control" required><br>
+						<input type="password" id="password" placeholder="Password" class="form-control" required><br>
+						<input type="password" id="password" placeholder="Confirm Password" class="form-control" required><br>
+						<input type="submit" value="Submit" class="form-control">
 					</form> 
 				</div>
 				

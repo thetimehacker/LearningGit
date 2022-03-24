59c91193ae70c9bbdbf5cd261077aaf12df2d80d
shubham Kumar <triangleofcoding@gmail.com>
2022-03-25 03:58:14 +0530

    Changed the background

--- index.php
+++ index.php
@@ -71,7 +71,8 @@
 					    text-decoration: none;
 					    display: inherit;
 					    text-align: center;
-					    text-decoration: none;">
+					    text-decoration: none;
+					    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);">
 
 						Log In
 
--- login.php
+++ login.php
@@ -19,7 +19,7 @@
 			<div class="col-sm-4"></div>
 			
 			<div class="col-sm-4">
-				<div class="contain_form">
+				<div class="contain_login_form">
 					<div class="Heading">
 						<h2 style="margin-bottom: 20px;">Log In</h2>
 					</div>
@@ -51,7 +51,8 @@
 					    text-decoration: none;
 					    display: inherit;
 					    text-align: center;
-					    text-decoration: none;">
+					    text-decoration: none;
+					    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);">
 
 						Create Account
 
